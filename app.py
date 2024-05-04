b16c382515a4c37142b3614f491487c6e77b4d12
Vidath-02 <163777374+Vidath-02@users.noreply.github.com>
2024-05-05 02:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ sales_data = pd.read_excel("cleaned_dataset.xlsx", engine='openpyxl')
 # Define chart configurations
 charts_info = [
     {"type": "box", "x": "Sub-Category", "y": "Quantity", "title": "Box Plot", "color": "#FF5733"},  # Change color here
-    {"type": "bar", "x": "Ship Mode", "y": "Shipping Cost", "title": "Bar Chart", "color": "Ship Mode"},
+    {"type": "bar", "x": "Ship Mode", "y": "Shipping Cost", "title": "Bar Chart"},
     {"type": "pie", "names": "Order Priority", "title": "Donut Chart", "hole": 0.5},
     {"type": "histogram", "x": "Sales", "title": "Histogram"},
     {"type": "area", "x": "Market", "y": "Profit", "title": "Area Chart"},
