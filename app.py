2b5d7a1b48090508c1339ab83a70c491bf6fd908
Vidath-02 <163777374+Vidath-02@users.noreply.github.com>
2024-05-05 01:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown('<div class="center header"><h1>Global Sales Dashboard<h1></div>', u
 st.markdown('<div class="center subheader"><h3>Analyze Your Sales Data<h3></div>', unsafe_allow_html=True)
 
 # Read the data
-df = pd.read_excel("cleaned_dataset.xlsx", engine='openpyxl')
+sales_data = pd.read_excel("cleaned_dataset.xlsx", engine='openpyxl')
 
 
 # Define a function to create charts
